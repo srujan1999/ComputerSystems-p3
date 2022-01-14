c5cada3f7972d1b9d22662ded0ddbc9d2b114cea
LaxmiNarayanaMurthyVemuri <murthyvemuri@msitprogram.net>
2022-01-14 11:55:22 +0530

    first commit

---
+++ proxy.py
@@ -0,0 +1,23 @@
+'''
+    Disclaimer
+    tiny httpd is a web server program for instructional purposes only
+    It is not intended to be used as a production quality web server
+    as it does not fully in compliance with the 
+    HTTP RFC https://tools.ietf.org/html/rfc2616
+
+'''
+
+class HTTPServer:
+    '''
+        Remove the pass statement below and write your code here
+    '''
+    pass
+
+def main():
+    # test harness checks for your web server on the localhost and on port 8888
+    # do not change the host and port
+    # you can change  the HTTPServer object if you are not following OOP
+    HTTPServer('127.0.0.1', 8888)
+
+if __name__ == "__main__":
+    main()
