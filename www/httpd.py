7ed34952feb0cb02b66c43b080f1f13ab419c24d
LaxmiNarayanaMurthyVemuri <murthyvemuri@msitprogram.net>
2022-01-14 11:56:56 +0530

    added www folder

---
+++ www/httpd.py
@@ -0,0 +1,15 @@
+
+class HTTPServer:
+    '''
+        Remove the pass statement below and write your code here
+    '''
+    pass
+
+def main():
+    # test harness checks for your web server on the localhost and on port 8888
+    # do not change the host and port
+    # you can change  the HTTPServer object if you are not following OOP
+    HTTPServer('127.0.0.1', 8888)
+
+if __name__ == "__main__":
+    main()                   
